217f206297332fea6633ef3125a70cb52a76a9e5
jcprice12 <jcprice12@gmail.com>
2017-09-10 16:12:41 -0400

    seeding works

--- dbResources/turnCsvToJson.js
+++ dbResources/turnCsvToJson.js
@@ -1,6 +1,26 @@
 var fs = require("fs");
 var path = require("path");
 
+function convertWord(word){
+    var numberVal = parseFloat(word);
+    if(numberVal){
+        word = numberVal;
+    } else {
+        switch(word.toLowerCase()){
+            case "":
+                word = null;
+                break;
+            case "true":
+                word = true;
+                break;
+            case "false":
+                word = false;
+                break;
+        }
+    }
+    return word;
+}
+
 module.exports = function(fileName){
     var data = fs.readFileSync(path.join(__dirname,fileName), 'utf8');
     var seeds = [];
@@ -16,6 +36,7 @@ module.exports = function(fileName){
                     nameOfCols.push(word);
                     word = "";
                 } else if(data[i] === "\n") {
+                    nameOfCols.push(word);
                     lineCounter++;
                     word="";
                 } else {
@@ -23,26 +44,11 @@ module.exports = function(fileName){
                 }
             } else {
                 if(data[i] === ','){
-                    var numberVal = parseFloat(word);
-                    if(numberVal){
-                        word = numberVal;
-                    } else {
-                        switch(word.toLowerCase()){
-                            case "":
-                                word = null;
-                                break;
-                            case "true":
-                                word = true;
-                                break;
-                            case "false":
-                                word = false;
-                                break;
-                        }
-                    }
-                    myObj[(nameOfCols[recordIndex])] = word;
+                    myObj[(nameOfCols[recordIndex])] = convertWord(word);
                     word = "";
                     recordIndex++;
                 } else if(data[i] === "\n") {
+                    myObj[(nameOfCols[recordIndex])] = convertWord(word);
                     seeds.push(myObj);
                     myObj = {};
                     lineCounter++;
--- seeders/20170910173058-pokemon.js
+++ seeders/20170910173058-pokemon.js
@@ -8,13 +8,6 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-    return queryInterface.bulkDelete('Pokemon');
+    return queryInterface.bulkDelete('Pokemon', null, {});
   }
 };
