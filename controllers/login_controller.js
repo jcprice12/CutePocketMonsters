445252979efa8c33842217bd0ccea9a5dd424c6a
jcprice12 <jcprice12@gmail.com>
2017-09-11 11:08:00 -0400

    added register user functionality

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var serverFile = require("../server.js");
-
+var password_hash = require("password-hash");
 // Import models
 var db = require("../models");
+
+function hashPassword(pass){
+    var hash = password_hash.generate(pass, {
+        algorithm : 'sha256',
+        saltLength : 16,
+    });
+    return hash;
+}
 
 //route for login page
 router.get("/login", function(req, res) {
@@ -12,9 +20,27 @@ router.get("/login", function(req, res) {
 });
 
 router.post("/login", serverFile.getPassport().authenticate('local', { failureRedirect: '/login' }), function(req, res) {
-      res.redirect('/');
-    }
-);
+    res.redirect('/');
+});
+
+router.get("/register", function(req, res){
+    var hbsObject = {}
+    res.render("register", hbsObject);
+});
+
+router.post("/register", function(req, res){
+    req.body.password = hashPassword(req.body.password);
+    db.User.create(req.body).then(function(userInserted){
+        req.login(userInserted, function(err) {
+            if(err){
+                console.log(err);
+                res.send({redirect: '/register'});
+            } else {
+                res.send({redirect: '/'});
+            }
+        });
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
