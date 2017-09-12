49ca0b39dbcfdbdcb5fb3ecbd6c30ed42215bb73
jcprice12 <jcprice12@gmail.com>
2017-09-12 19:04:19 -0400

    bug fixes, started adding edit user functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,19 +5,67 @@ var connectEnsureLogin = require("connect-ensure-login");
 // Import models
 var db = require("../models");
 
-router.get("/users/:id?", serverFile.checkUser, function(req, res) {
-    if(req.params.id){
-        db.User.findOne({
-            where : {
-                "id" : req.params.id
-            },
-            include : {
-                model : db.Pokemon,
-                through : {
-                    attributes : ['pokemonNumber']
-                } 
+var getUserAndPokemon = function(userId){
+    var promise = db.User.findOne({
+        where : {
+            "id" : userId
+        },
+        include : {
+            model : db.Pokemon,
+            through : {
+                attributes : ['pokemonNumber', 'starting']
             }
-        }).then(function(userPokemon){
+        } 
+    });
+    return promise;
+}
+
+router.get("/users/similar/:id", serverFile.checkUser, function(req, res){
+    var query = "";
+    query += "SELECT t3.id, t3.username, t3.email, count(*) as numberOfSimilarPokemon";
+    query += " FROM userpokemon AS t1";
+    query += " INNER JOIN (SELECT  pokemonNumber FROM userPokemon WHERE userId = :userId) AS t2 ON t1.pokemonNumber = t2.pokemonNumber";
+    query += " INNER JOIN users AS t3 ON t1.userId = t3.id";
+    query += " WHERE t1.userId <> :userId";
+    query += " GROUP BY userId";
+    query += " ORDER BY numberOfSimilarPokemon DESC";
+    db.sequelize.query(query, {
+        replacements : {
+            userId : req.params.id
+        },
+        type: db.sequelize.QueryTypes.SELECT
+    }).then(function(data){
+        console.log(data);
+        var hbsObject = {
+            sessionUser : req.user,
+            similarTrainers : data
+        };
+        res.render("allUsers", hbsObject);
+    });
+});
+
+router.get("/users/edit", serverFile.checkUser, function(req, res){
+    getUserAndPokemon(req.user.dataValues.id).then(function(userPokemon){
+        var hbsObject = {
+            "userPokemon" : userPokemon,
+            "sessionUser" : req.user
+        }
+        console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        console.log(hbsObject.userPokemon.dataValues);
+        console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5");
+        console.log(hbsObject.userPokemon.dataValues.Pokemons[0].dataValues.UserPokemon.dataValues.starting);
+        res.render("editUser", hbsObject);
+    });
+});
+
+router.get("/users/:id?", serverFile.checkUser, function(req, res, next) {
+    
+    if(isNaN(req.params.id)){
+       return next();
+    }
+
+    if(req.params.id){
+        getUserAndPokemon(req.params.id).then(function(userPokemon){
             var hbsObject = {
                 "userPokemon" : userPokemon,
                 "sessionUser" : req.user
@@ -43,30 +91,6 @@ router.get("/users/:id?", serverFile.checkUser, function(req, res) {
             res.render("allUsers", hbsObject);
         });
     }
-});
-
-router.get("/users/similar/:id", serverFile.checkUser, function(req, res){
-    var query = "";
-    query += "SELECT t3.id, t3.username, t3.email, count(*) as numberOfSimilarPokemon";
-    query += " FROM userpokemon AS t1";
-    query += " INNER JOIN (SELECT  pokemonNumber FROM userPokemon WHERE userId = :userId) AS t2 ON t1.pokemonNumber = t2.pokemonNumber";
-    query += " INNER JOIN users AS t3 ON t1.userId = t3.id";
-    query += " WHERE t1.userId <> :userId";
-    query += " GROUP BY userId";
-    query += " ORDER BY numberOfSimilarPokemon DESC";
-    db.sequelize.query(query, {
-        replacements : {
-            userId : req.params.id
-        },
-        type: db.sequelize.QueryTypes.SELECT
-    }).then(function(data){
-        console.log(data);
-        var hbsObject = {
-            sessionUser : req.user,
-            similarTrainers : data
-        };
-        res.render("allUsers", hbsObject);
-    });
 });
 
 // Export routes for server.js to use.
