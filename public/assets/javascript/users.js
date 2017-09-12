88fd4a5d1f59c8eb8771bdbc2e642eefdc4499a7
jcprice12 <jcprice12@gmail.com>
2017-09-11 20:12:26 -0400

    bug fixes for all trainers page

--- public/assets/javascript/users.js
+++
@@ -1,9 +0,0 @@
-console.log("yah");
-$(document).ready(function(){
-    console.log("ready");
-    $(".myTr").click(function(){
-        console.log("hello");
-        var userId = $(this).attr("data-userId");
-        console.log(userId);
-    });
-});
